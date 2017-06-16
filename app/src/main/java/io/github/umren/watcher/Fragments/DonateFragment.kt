cc2e27706b7df15392c023009d0c8d07c75fe0f4
Ruiz Aw <umren@yandex.ru>
2017-06-16 12:46:20 +0300

    DialogFragment now have links, but formatting is broken

--- app/src/main/java/io/github/umren/watcher/Fragments/DonateFragment.kt
+++ app/src/main/java/io/github/umren/watcher/Fragments/DonateFragment.kt
@@ -4,15 +4,22 @@ import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import io.github.umren.watcher.R
 
 
 class DonateFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val message = TextView(context)
+        message.text = context.getText(R.string.fragment_donate)
+        message.movementMethod = LinkMovementMethod.getInstance()
+
         return AlertDialog.Builder(activity)
                 .setTitle("Donate")
-                .setMessage(resources.getString(R.string.fragment_donate))
+                .setView(message)
                 .setPositiveButton(android.R.string.ok, null)
                 .create()
     }
